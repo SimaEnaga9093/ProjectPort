be3dd71eedcc44b5def4de025f4d4a78fa88c9a4
SimaEnaga <vharugun9093@gmail.com>
2023-12-07 18:03:06 +0900

    Initial Unreal Project

---
+++ Source/ProjectPort/ProjectPortGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectPortGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTPORT_API AProjectPortGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
