257b827eb24aa7d0f6b5cb53affdacea6e32778c
SimaEnaga <vharugun9093@gmail.com>
2023-12-18 22:59:17 +0900

    Create Manage Employ Popup

---
+++ Source/ProjectPort/UI/Manage/PManageEmployPopupWidget.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "../Module/PPopupWidget.h"
+
+#include "PManageEmployPopupWidget.generated.h"
+
+/**
+ * Manage - Employ Popup Widget
+ */
+UCLASS()
+class PROJECTPORT_API UPManageEmployPopupWidget : public UPPopupWidget
+{
+	GENERATED_BODY()
+
+};
