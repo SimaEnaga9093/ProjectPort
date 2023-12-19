02ec16b4ba0682227ab2d6d13de77b7b64415dc1
SimaEnaga <vharugun9093@gmail.com>
2023-12-19 21:03:24 +0900

    Bind EmployPopup Widget

--- Source/ProjectPort/UI/Manage/PManageEmployPopupWidget.cpp
+++ Source/ProjectPort/UI/Manage/PManageEmployPopupWidget.cpp
@@ -2,3 +2,105 @@
 
 
 #include "PManageEmployPopupWidget.h"
+
+#include "Components/EditableTextBox.h"
+#include "Components/Button.h"
+#include "../Module/PCommonButton.h"
+
+void UPManageEmployPopupWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (EditableTextName)
+		EditableTextName->OnTextChanged.AddDynamic(this, &UPManageEmployPopupWidget::OnEditableTextNameChanged);
+
+	if (ButtonJobTanker)
+		ButtonJobTanker->OnClicked.AddDynamic(this, &UPManageEmployPopupWidget::OnButtonJobTankerClicked);
+	if (ButtonJobMelee)
+		ButtonJobMelee->OnClicked.AddDynamic(this, &UPManageEmployPopupWidget::OnButtonJobMeleeClicked);
+	if (ButtonJobRange)
+		ButtonJobRange->OnClicked.AddDynamic(this, &UPManageEmployPopupWidget::OnButtonJobRangeClicked);
+	if (ButtonJobHealer)
+		ButtonJobHealer->OnClicked.AddDynamic(this, &UPManageEmployPopupWidget::OnButtonJobHealerClicked);
+
+	if (EditableTextSTR)
+		EditableTextSTR->OnTextChanged.AddDynamic(this, &UPManageEmployPopupWidget::OnEditableTextSTRChanged);
+	if (EditableTextDEX)
+		EditableTextDEX->OnTextChanged.AddDynamic(this, &UPManageEmployPopupWidget::OnEditableTextDEXChanged);
+	if (EditableTextINT)
+		EditableTextINT->OnTextChanged.AddDynamic(this, &UPManageEmployPopupWidget::OnEditableTextINTChanged);
+	if (EditableTextMND)
+		EditableTextMND->OnTextChanged.AddDynamic(this, &UPManageEmployPopupWidget::OnEditableTextMNDChanged);
+
+	if (CommonButtonEmploy)
+		CommonButtonEmploy->GetButtonBG()->OnClicked.AddDynamic(this, &UPManageEmployPopupWidget::OnCommonButtonEmployClicked);
+}
+
+void UPManageEmployPopupWidget::NativeDestruct()
+{
+	Super::NativeDestruct();
+
+	if (EditableTextName)
+		EditableTextName->OnTextChanged.RemoveAll(this);
+
+	if (ButtonJobTanker)
+		ButtonJobTanker->OnClicked.RemoveAll(this);
+	if (ButtonJobMelee)
+		ButtonJobMelee->OnClicked.RemoveAll(this);
+	if (ButtonJobRange)
+		ButtonJobRange->OnClicked.RemoveAll(this);
+	if (ButtonJobHealer)
+		ButtonJobHealer->OnClicked.RemoveAll(this);
+
+	if (EditableTextSTR)
+		EditableTextSTR->OnTextChanged.RemoveAll(this);
+	if (EditableTextDEX)
+		EditableTextDEX->OnTextChanged.RemoveAll(this);
+	if (EditableTextINT)
+		EditableTextINT->OnTextChanged.RemoveAll(this);
+	if (EditableTextMND)
+		EditableTextMND->OnTextChanged.RemoveAll(this);
+
+	if (CommonButtonEmploy)
+		CommonButtonEmploy->GetButtonBG()->OnClicked.RemoveAll(this);
+}
+
+void UPManageEmployPopupWidget::OnEditableTextNameChanged(const FText& Text)
+{
+}
+
+void UPManageEmployPopupWidget::OnButtonJobTankerClicked()
+{
+}
+
+void UPManageEmployPopupWidget::OnButtonJobMeleeClicked()
+{
+}
+
+void UPManageEmployPopupWidget::OnButtonJobRangeClicked()
+{
+}
+
+void UPManageEmployPopupWidget::OnButtonJobHealerClicked()
+{
+}
+
+void UPManageEmployPopupWidget::OnEditableTextSTRChanged(const FText& Text)
+{
+}
+
+void UPManageEmployPopupWidget::OnEditableTextDEXChanged(const FText& Text)
+{
+}
+
+void UPManageEmployPopupWidget::OnEditableTextINTChanged(const FText& Text)
+{
+}
+
+void UPManageEmployPopupWidget::OnEditableTextMNDChanged(const FText& Text)
+{
+}
+
+void UPManageEmployPopupWidget::OnCommonButtonEmployClicked()
+{
+}
--- Source/ProjectPort/UI/Manage/PManageEmployPopupWidget.h
+++ Source/ProjectPort/UI/Manage/PManageEmployPopupWidget.h
@@ -8,6 +8,10 @@
 
 #include "PManageEmployPopupWidget.generated.h"
 
+class UEditableTextBox;
+class UButton;
+class UPCommonButton;
+
 /**
  * Manage - Employ Popup Widget
  */
@@ -16,4 +20,55 @@ class PROJECTPORT_API UPManageEmployPopupWidget : public UPPopupWidget
 {
 	GENERATED_BODY()
 
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+	UFUNCTION()
+	void OnEditableTextNameChanged(const FText& Text);
+
+	UFUNCTION()
+	void OnButtonJobTankerClicked();
+	UFUNCTION()
+	void OnButtonJobMeleeClicked();
+	UFUNCTION()
+	void OnButtonJobRangeClicked();
+	UFUNCTION()
+	void OnButtonJobHealerClicked();
+
+	UFUNCTION()
+	void OnEditableTextSTRChanged(const FText& Text);
+	UFUNCTION()
+	void OnEditableTextDEXChanged(const FText& Text);
+	UFUNCTION()
+	void OnEditableTextINTChanged(const FText& Text);
+	UFUNCTION()
+	void OnEditableTextMNDChanged(const FText& Text);
+
+	UFUNCTION()
+	void OnCommonButtonEmployClicked();
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TSoftObjectPtr<UEditableTextBox> EditableTextName;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TSoftObjectPtr<UButton> ButtonJobTanker;
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TSoftObjectPtr<UButton> ButtonJobMelee;
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TSoftObjectPtr<UButton> ButtonJobRange;
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TSoftObjectPtr<UButton> ButtonJobHealer;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TSoftObjectPtr<UEditableTextBox> EditableTextSTR;
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TSoftObjectPtr<UEditableTextBox> EditableTextDEX;
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TSoftObjectPtr<UEditableTextBox> EditableTextINT;
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TSoftObjectPtr<UEditableTextBox> EditableTextMND;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TSoftObjectPtr<UPCommonButton> CommonButtonEmploy;
 };
