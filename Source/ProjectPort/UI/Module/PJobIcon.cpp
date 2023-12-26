08b5259514f10c8faee9cb5c42742d468fc8c478
SimaEnaga <vharugun9093@gmail.com>
2023-12-26 18:36:24 +0900

    Create Module Job icon

---
+++ Source/ProjectPort/UI/Module/PJobIcon.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PJobIcon.h"
+
+#include "Components/SizeBox.h"
+#include "Components/Image.h"
+
+void UPJobIcon::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	SizeBoxBG->SetWidthOverride(WidthHeightOverride.X);
+	SizeBoxBG->SetHeightOverride(WidthHeightOverride.Y);
+
+	ImageIcon->SetBrushFromTexture(IconJobImages[JobType].LoadSynchronous());
+}
---
+++ Source/ProjectPort/UI/Module/PJobIcon.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "PUserWidget.h"
+#include "../../Data/PContentCharacterInfo.h"
+
+#include "PJobIcon.generated.h"
+
+class USizeBox;
+class UImage;
+class UTexture2D;
+
+/**
+ * Module Job Icon with EContentCharacterJob
+ */
+UCLASS()
+class PROJECTPORT_API UPJobIcon : public UPUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativePreConstruct() override;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TSoftObjectPtr<USizeBox> SizeBoxBG;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TSoftObjectPtr<UImage> ImageIcon;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TMap<EContentCharacterJob, TSoftObjectPtr<UTexture2D>> IconJobImages;
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FVector2D WidthHeightOverride;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	EContentCharacterJob JobType;
+};
